ceaa569edc2bdda6861ffe5fbb24846cb20d7459
shalomitaimanuellamutiarani <imanuellamutiarani@gmail.com>
2024-11-20 09:29:53 +0700

    adding name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Rechie Lagria</h1>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Shalomita Imanuella</h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a frontend Developer</h2>
         <p className=' text-[#8892b0] py-4 max-w-[700px]'>I'm an IT Support and a Frontend Developer from the Philippines. I love computers and technology particulary in coding.My specialties includes Javascript,Reactjs, Nextjs, NodeJS and many more.I'm passionate about learning, expanding knowledge and building high quality software and web applications.</p>
         <div>
